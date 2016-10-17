cca49eaf4547dd9ad16e7cd5fc449b191aa30344
mignonstyle <mignonxstyle@gmail.com>
2016-10-18 04:04:50 +0900

    add test functions

--- tests/ArithmeticTest.php
+++ tests/ArithmeticTest.php
@@ -20,33 +20,47 @@ class ArithmeticTest extends PHPUnit_Framework_TestCase {
 		$this->object = new Arithmetic();
 	}
 
-
 	/**
 	 * 足し算関数の検証
 	 */
 	public function testAdd() {
-		// 引数に3,5を渡すと8が帰ってくることを確認する
+		// 引数に3, 5を渡すと8が帰ってくることを確認する
 		$this->assertEquals( 8, $this->object->add( 3, 5 ) );
 
 		// 引数に15, 30を渡すと45が帰ってくることを確認する
 		$this->assertEquals( 45, $this->object->add( 15, 30 ) );
 	}
 
+	/**
+	 * 引き算関数の検証
+	 */
+	public function testSubtract() {
+		// 引数に10, 3を渡すと7が帰ってくることを確認する
+		$this->assertEquals( 7, $this->object->subtract( 10, 3 ) );
 
+		// 引数に3, 9を渡すと-6が帰ってくることを確認する
+		$this->assertEquals( -6, $this->object->subtract( 3, 9 ) );
+	}
 
+	/**
+	 * 掛け算関数の検証
+	 */
+	public function testMultiply() {
+		// 引数に4, 6を渡すと24が帰ってくることを確認する
+		$this->assertEquals( 24, $this->object->multiply( 4, 6 ) );
 
+		// 引数に4, -5を渡すと-20が帰ってくることを確認する
+		$this->assertEquals( -20, $this->object->multiply( 4, -5 ) );
+	}
 
+	/**
+	 * 割り算関数の検証
+	 */
+	public function testDivide() {
+		// 引数に6, 2を渡すと3が帰ってくることを確認する
+		$this->assertEquals( 3, $this->object->divide( 6, 2 ) );
 
-
-
+		// 引数に6, 6を渡すと1が帰ってくることを確認する
+		$this->assertEquals( 1, $this->object->divide( 6, 6 ) );
+	}
 }
-
-
-
-
-
-
-
-
-
- ?>
