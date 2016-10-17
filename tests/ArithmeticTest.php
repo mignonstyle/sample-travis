c9478598c81db80350594b0403ea06071096ef15
mignonstyle <mignonxstyle@gmail.com>
2016-10-18 03:38:39 +0900

    Modify arithmetic test

--- tests/ArithmeticTest.php
+++ tests/ArithmeticTest.php
@@ -6,6 +6,37 @@
 require_once 'Arithmetic.php';
 
 class ArithmeticTest extends PHPUnit_Framework_TestCase {
+	/**
+	 *
+	 */
+	protected $object;
+
+	/**
+	 *
+	 */
+	protected function setUp() {
+		//
+		$this->object = new Arithmetic();
+	}
+
+
+	/**
+	 *
+	 */
+	public function testAdd() {
+		//
+		$this->assertEquals( 8, $this->object->add( 3, 5 ) );
+
+		//
+		$this->assertEquals( 45, $this->object->add( 15, 30 ) );
+	}
+
+
+
+
+
+
+
 
 }
 
