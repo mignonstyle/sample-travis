ec0c6688729c30ac7520c921eaceb280ff2c1754
mignonstyle <mignonxstyle@gmail.com>
2016-10-18 03:28:38 +0900

    add arithmetic

---
+++ tests/Arithmetic.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * 四則演算を行うクラス
+ */
+
+class Arithmetic {
+	/**
+	 * 足し算
+	 */
+	public function add( $x, $y ) {
+		$ret = $x + $y;
+		return $ret;
+	}
+
+	/**
+	 * 引き算
+	 */
+	public function add( $x, $y ) {
+		$ret = $x - $y;
+		return $ret;
+	}
+
+	/**
+	 * 掛け算
+	 */
+	public function add( $x, $y ) {
+		$ret = $x * $y;
+		return $ret;
+	}
+
+	/**
+	 * 割り算
+	 */
+	public function add( $x, $y ) {
+		$ret = $x / $y;
+		return $ret;
+	}
+}
