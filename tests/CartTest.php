9b104f9879d658b520fd4580848842b7570bc581
mignonstyle <mignonxstyle@gmail.com>
2016-10-19 01:36:13 +0900

    add carttest functions

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -41,4 +41,26 @@ class CartTest extends PHPUnit_Framework_TestCase {
 		}
 		$this->fail();
 	}
+
+	public function testAddNotNumeric() {
+		$cart = new Cart();
+
+		try {
+			$cart->add( '001', 'string' );
+		} catch( OutOfBoundsException $e ) {
+			return;
+		}
+		$this->fail();
+	}
+
+	public function testAddFloat() {
+		$cart = new Cart();
+
+		try {
+			$cart->add( '001', 1.5 );
+		} catch( OutOfBoundsException $e ) {
+			return;
+		}
+		$this->fail();
+	}
 }
