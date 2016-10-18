3d5efe2c9687d4b111d0bb009e57285528cd7a6e
mignonstyle <mignonxstyle@gmail.com>
2016-10-19 06:14:24 +0900

    add carttest class limit and remove functions

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -17,7 +17,6 @@ class CartTest extends PHPUnit_Framework_TestCase {
 
 	public function testAdd() {
 		$cart = new Cart();
-
 		$this->assertTrue( $cart->add( '001', 1 ) );
 		$this->assertTrue( $cart->add( '001', 0 ) );
 		$this->assertTrue( $cart->add( '001', -1 ) );
@@ -88,5 +87,53 @@ class CartTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertTrue( is_array( $cart->getItems() ) );
 		$this->assertEquals( 0, count( $cart->getItems() ) );
+	}
+
+	public function testAddUpperLimit() {
+		$cart = new Cart();
+
+		$cart->add( '001', PHP_INT_MAX );
+
+		try {
+			$cart->add( '001', 1 );
+		} catch ( OutOfRangeException $e ) {
+			return;
+		}
+		$this->fail();
+	}
+
+	public function testAddUnderLimit() {
+		$cart = new Cart();
+
+		$cart->add( '001', 1 );
+		$this->assertEquals( 1, count( $cart->getItems() ) );
+		$cart->add( '001', -1 );
+		$this->assertEquals( 0, count( $cart->getItems() ) );
+
+		$cart->clear();
+		$cart->add( '001', 1 );
+		$this->assertEquals( 1, count( $cart->getItems() ) );
+		$cart->add( '001', -2 );
+		$this->assertEquals( 0, count( $cart->getItems() ) );
+	}
+
+	public function testAddRemove() {
+		$cart = new Cart();
+
+		$cart->add( '001', 2 );
+		$cart->add( '002', 3 );
+		$this->assertEquals( 2, count( $cart->getItems() ) );
+
+		$cart->add( '001', -2 );
+		$items = $cart->getItems();
+		$this->assertEquals( 1, count( $items ) );
+		$this->assertFalse( isset( $items['001'] ) );
+		$this->assertEquals( 3, $items['002'] );
+
+		$cart->add( '002', -3 );
+		$items = $cart->getItems();
+		$this->assertEquals( 0, count( $items ) );
+		$this->assertFalse( isset( $items['001'] ) );
+		$this->assertFalse( isset( $items['002'] ) );
 	}
 }
