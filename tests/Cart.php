b7f58e87dbb5e818d2493e6aa1286a5abb63f7fa
mignonstyle <mignonxstyle@gmail.com>
2016-10-19 05:23:43 +0900

    add test cart functions

--- tests/Cart.php
+++ tests/Cart.php
@@ -4,12 +4,22 @@
  * Cart
  */
 class Cart {
+	private $items;
+
+	public function __construct() {
+		$this->items = array();
+	}
+
 	public function getItems() {
 		return array();
 	}
 
 	public function add( $item_cd, $amount ) {
 		if ( preg_match( '/^-?\d+$/', $amount ) ) {
+			if ( !isset( $this->items[$item_cd] ) ) {
+				$this->items[$item_cd] = 0;
+			}
+			$this->items[$item_cd] += ( int )$amount;
 			return true;
 		} else {
 			throw new UnexpectedValueException( 'Invalid amount' );
@@ -17,6 +27,10 @@ class Cart {
 	}
 
 	public function getAmount( $item_cd ) {
-		
+		if ( isset( $this->items[$item_cd] ) ) {
+			return $this->items[$item_cd];
+		} else {
+			return 0;
+		}
 	}
 }
